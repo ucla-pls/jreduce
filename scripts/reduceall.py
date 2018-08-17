dd244f6272b037646e5934627ababf4c1018daa3
Christian Gram Kalhauge <christian@kalhauge.dk>
2018-08-17 10:39:01 +0200

    Fix reduceall

--- scripts/reduceall.py
+++ scripts/reduceall.py
@@ -5,7 +5,7 @@ import csv
 import sys
 import subprocess
 from multiprocessing import Pool
-from subprocess import DEVNULL, call, check_call, Popen, PIPE, STDOUT, check_output
+from subprocess import DEVNULL, call, check_call, Popen, PIPE, STDOUT, check_output, TimeoutExpired
 from shutil import rmtree
 from pathlib import Path
 
@@ -17,28 +17,32 @@ logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler(sys.stderr)
 ch.setLevel(logging.DEBUG)
 # create formatter and add it to the handlers
-formatter = logging.Formatter('(%(asctime)s): %(message)s')
+formatter = logging.Formatter('(%(asctime)s)-%(processName)s: %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 info = logger.info
 debug = logger.debug
 
-jreduce_folder = Path(os.path.realpath(__file__)).parent.parent
-test_suite = jreduce_folder / "test-suite"
-reductors = [ "ddmin", "ddmin:graph", "gbired", "ddmin:verify"]
+reductors = [ "ddmin:graph", "gbired", "ddmin:verify"]
 
 def run_jreduce(reductor, classes, work_dir, prop, max_iterations=1000):
-    call(
-        ["jreduce", "-v",
-         "--cp", str(classes),
-         "-r", reductor,
-         "--work-dir", str(work_dir),
-         "-m", str(max_iterations),
-        ] + prop)
+    info("Running {}".format(work_dir))
+    try:
+        call(
+            ["jreduce", "-v",
+            "--cp", str(classes),
+            "-p", "class",
+            "-r", reductor,
+            "--work-dir", str(work_dir),
+            "-m", str(max_iterations),
+            ] + prop, stdout=DEVNULL, stderr=DEVNULL, timeout=3600)
+    except Exception as e:
+        info("{}: {}".format(e, work_dir))
+    info("Done running {}".format(work_dir))
 
 
-def compute_reduction(jar, prop, output_folder):
+def compute_reduction(jar, prop, output_folder, pool):
     classes = output_folder / "classes"
     classes.mkdir(parents=True)
 
@@ -48,7 +52,8 @@ def compute_reduction(jar, prop, output_folder):
 
     stdout = output_folder / "stdout.log"
     with open(str(stdout), "wb") as out:
-        p = Popen(["bash", str(prop.resolve()), "classes"], stderr=STDOUT, stdout=PIPE, cwd=str(output_folder))
+        p = Popen(["bash", str(prop.resolve()), "classes"],
+                  stderr=STDOUT, stdout=PIPE, cwd=str(output_folder))
         for line in p.stdout:
             out.write(line)
         n = p.wait()
@@ -56,27 +61,22 @@ def compute_reduction(jar, prop, output_folder):
             info("Property did not succeed; return code = {}".format(n))
             return
         else:
-            info("Property succeded, see stdout: {}".format(stdout.relative_to(Path.cwd())))
+            info("Property succeded, see stdout: {}".format(stdout))
 
-    info("Running on reductors:")
     for red in reductors:
-        info("Running {}".format(red))
-        run_jreduce(red, classes, output_folder / red, [str(prop)])
+        pool.apply_async(run_jreduce, (red, classes, output_folder / red, [str(prop)]))
 
-def analyse_jar(jar, prop):
+def analyse_jar(jar, prop, basefolder, pool):
     name = jar.stem
     prop_name = prop.stem
-    output_folder = test_suite / prop_name / name
+    output_folder = basefolder / prop_name / name
 
-    if output_folder.exists():
-        info("Already did {}".format(name))
-    else:
+    if not output_folder.exists():
         info("Working on {}".format(name))
-        compute_reduction(jar, prop, output_folder)
+        compute_reduction(jar, prop, output_folder, pool)
+        return None
 
-
-
-    dct = { "name" : name}
+    dct = { "name" : name, "prop": prop_name}
 
     for red in reductors:
         iters = 0
@@ -84,49 +84,55 @@ def analyse_jar(jar, prop):
         score = 0
         best_score = 0
 
-
-        with open(str(output_folder / red / "progress.csv")) as c:
-            for line in csv.DictReader(c):
-                if "class-closure-" + red in line["Step"]:
-                    iters += 1
-                if "after-class-closure" in line["Step"]:
-                    final = line["Classes"]
-                    diffname = output_folder / red / "after-class-closure.diff"
-                    call(
-                        "diff --left-column --side-by-side {} {} > {}"
-                        .format(output_folder / "stdout.log",
-                                output_folder / red / line["Step"] / "stderr.log",
-                                diffname),
-                        shell=True)
-                    with open(str(diffname)) as r:
-                        for line in r:
-                            best_score +=1
-                            if line.endswith("(\n") or line.endswith("<\n"):
-                                score +=1
-
+        try:
+            with open(str(output_folder / red / "progress.csv")) as c:
+                for line in csv.DictReader(c):
+                    if "class-closure-" + red in line["Step"]:
+                        iters += 1
+                    if "after-class-closure" in line["Step"]:
+                        final = line["Classes"]
+                        diffname = output_folder / red / "after-class-closure.diff"
+                        call(
+                            "diff --left-column --side-by-side {} {} > {}"
+                            .format(output_folder / "stdout.log",
+                                    output_folder / red / line["Step"] / "stderr.log",
+                                    diffname),
+                            shell=True)
+                        with open(str(diffname)) as r:
+                            for line in r:
+                                best_score +=1
+                                if line.endswith("(\n") or line.endswith("<\n"):
+                                    score +=1
+        except:
+            pass
+        
         dct[red + " iter"] = iters
         dct[red + " size"] = final
         dct[red + " change"] = 0 if best_score == 0 else score / best_score
 
-    p = Popen("javaq -f csv --cp {}".format(jar), stdout=PIPE, universal_newlines=True, shell=True)
-    classes = list(csv.DictReader(p.stdout))
-    p.wait();
+    classes = list((output_folder / "classes").glob("**/*.class"))
     dct["classes"] = len(classes)
 
     return dct
 
 
 if __name__ == "__main__":
-    cmd, prop, *jars = sys.argv
+    cmd, basefolder, *args = sys.argv
+
+    i = args.index("--")
+    props = [Path(prop) for prop in args[:i]]
+    jars = [Path(jar) for jar in args[i+1:]]
 
     writer = csv.DictWriter(sys.stdout,
-            ["name", "classes"] +
+            ["name", "prop", "classes"] +
                             sum([[red + " " + kind for red in reductors ] for  kind in ["iter", "size", "change"]], []))
     writer.writeheader()
 
-    def run(jar):
-        return analyse_jar(Path(jar), Path(prop))
-
     with Pool() as p:
-        for row in p.map(run, jars):
-            writer.writerow(row)
+        for jar in jars:
+            for prop in props:
+                r = analyse_jar(jar, prop, Path(basefolder), p)
+                if r:
+                    writer.writerow(r)
+        p.close()
+        p.join()
