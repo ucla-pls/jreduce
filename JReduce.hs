f81e89a16019b73a57144537f8ef676eeb54d458
Christian Gram Kalhauge <christian@kalhauge.dk>
2018-12-07 13:22:42 -0800

    Slightly improved version

--- JReduce.hs
+++ JReduce.hs
@@ -1,40 +1,63 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TupleSections       #-}
+{-# LANGUAGE ViewPatterns        #-}
 
 -- lens
 import           Control.Lens
--- import Control.Lens.TH
+
+-- deepseq
+import           Control.DeepSeq
+
+-- zip-archive
+import           Codec.Archive.Zip
 
 -- mtl
 import           Control.Monad.Reader
 
 -- filepath
-import System.FilePath
+import           System.FilePath
 
 -- optparse-applicative
 import           Options.Applicative          as A
 
 -- reduce
-import           Control.Reduce
+import           Data.Functor.Contravariant.PredicateM
+-- import           Control.Reduce
+
+-- bytestring
+import qualified Data.ByteString.Lazy         as BL
 
 -- reduce-util
-import           System.Directory.Tree
 import           Control.Reduce.Util
 import           Control.Reduce.Util.Logger   as L hiding (update, view)
 import           Control.Reduce.Util.OptParse
+import           System.Directory.Tree
 
 -- unordered-containers
-import qualified Data.HashSet                 as S
--- import qualified Data.HashMap.Strict as M
+import qualified Data.HashMap.Strict          as HashMap
+import qualified Data.HashSet                 as HashSet
+
+-- -- containers
+-- import qualified Data.Map                     as Map
+-- import qualified Data.Set                     as Set
+
+-- text
+-- import qualified Data.Text.Lazy.Builder as Builder
 
 -- unliftio
-import  UnliftIO
-import  UnliftIO.Directory
+import           UnliftIO.Directory
 
 -- base
+-- import           Data.Either
 import           Data.Foldable
+import qualified Data.List                    as List
 import           Data.Maybe
+import           System.Exit
 
 -- jvhms
 import           Jvmhs
@@ -46,11 +69,12 @@ instance Show (Showed x) where
 
 data Config = Config
   { _cfgLogger         :: !Logger
-  , _cfgCore           :: S.HashSet ClassName
+  , _cfgCore           :: HashSet.HashSet ClassName
   , _cfgClassPath      :: [ FilePath ]
   , _cfgUseStdlib      :: Bool
   , _cfgJreFolder      :: (Maybe FilePath)
   , _cfgTarget         :: FilePath
+  , _cfgOutput         :: FilePath
   , _cfgReducerOptions :: !ReducerOptions
   , _cfgCheckOptions   :: !CheckOptions
   , _cfgCmdOptions     :: !(Showed CmdOptionWithoutFormat)
@@ -92,6 +116,12 @@ configParser =
       <> metavar "FILE"
       <> help "the path to the jar or folder to reduce."
     )
+    <*> strOption
+    ( short 'o'
+      <> long "output"
+      <> metavar "FILE"
+      <> help "the path output folder."
+    )
     <*> parseReducerOptions "jreduce"
     <*> parseCheckOptions
     <*> parseCmdOptions
@@ -103,17 +133,17 @@ configParser =
       <> help "the core classes to not reduce."
       )
 
-    mkConfig l cores cp stdlib jre target comRed co c = do
+    mkConfig l cores cp stdlib jre target output comRed co c = do
       classCore <- readClassNames cores
       red <- comRed
-      return $ Config l classCore cp stdlib jre target red co (Showed c)
+      return $ Config l classCore cp stdlib jre target output red co (Showed c)
 
     readClassNames classnames' = do
       names <- fmap concat . forM classnames' $ \cn ->
         case cn of
           '@':filename -> lines <$> readFile filename
           _            -> return [cn]
-      return . S.fromList . map strCls $ names
+      return . HashSet.fromList . map strCls $ names
 
 main :: IO ()
 main = do
@@ -124,45 +154,155 @@ main = do
     <> progDesc "A command line tool for reducing java programs."
     )
 
-  print cfg
   runReaderT run cfg
 
 run :: ReaderT Config IO ()
 run = do
-  targets <- readTargetClasses
+  workFolder <- view $ cfgReducerOptions . to workFolder
+
+  (ttree, textra) <- unpackTarget =<< makeAbsolute (workFolder </> "unpacked")
+
+  let
+    targets = catMaybes $
+        (\(fn, a) -> (,a) <$> asClassName fn)
+        <$> toFileList ttree
+    classesToFiles clss =
+      textra <> (
+        fromJust . fromFileList . map (_1 %~ relativePathOfClass) $ clss
+      )
+
   classreader <- preloadClasses
 
   void . flip runCachedClassPoolT (defaultFromReader classreader) $ do
-    predicate <- setupPredicate targets
+    setupPredicate classesToFiles targets >>= \case
+      Just predicate -> do
+        classReduction predicate targets >>= \case
+          Just t' -> do
+            -- L.info "Is a cool thing "
+            output <- view cfgOutput
+            liftIO . writeTreeWith copySame $
+              output :/ (textra <> (fromJust $ fromFileList (map (over _1 relativePathOfClass) t')))
+          Nothing -> do
+            L.err "Could not reduce results."
+            liftIO $ exitWith (ExitFailure 1)
+      Nothing -> do
+        L.err "Could not satisfy predicate."
+        liftIO $ exitWith (ExitFailure 1)
 
-    return ()
+  where
+    copySame fp = \case
+      SameAs fp' -> copyFile fp' fp
+      Content bs -> BL.writeFile fp bs
+
+classReduction ::
+  forall m env a.
+  (HasLogger env, HasConfig env, MonadReader env m, MonadClassPool m, MonadIO m)
+  => PredicateM (ReaderT env IO) [(ClassName, a)]
+  -> [(ClassName, a)]
+  -> m (Maybe [(ClassName, a)])
+classReduction predicate targets = L.phase "Class Reduction" $ do
+  (coreFp, grph) <- L.phase "Compute Class Graph" $ do
+    (coreCls, grph) <- forwardRemove <$> mkClassGraph <*> view cfgCore
+    let coreFp = toValues coreCls
+    L.debug
+      $ "The core closure is" <-> display (length coreCls)
+      <-> "classes," <-> display (length coreFp) <-> "in the target."
+    return (coreFp, grph)
+
+  let graphLeft = graphSize grph
+  if graphLeft == 0
+    then do
+    L.debug $ "No further class reduction after core closure."
+    return (Just targets)
+
+    else do
+    L.debug $ "Possible reduction left:" <-> display graphLeft
+    partitions <- L.phase "Compute partition of graph:" $ do
+      let partitions = partition grph
+      L.debug $ "Found" <-> display (length partitions) <-> "SCC."
+      return $!! partitions
+
+    redOpt <- view cfgReducerOptions
+    liftRIO $ do
+      x <- reduce redOpt "class" (logAndTransform coreFp `contramapM` predicate) $ partitions
+      return $ fmap (\a -> coreFp ++ toX a) x
+
+  where
+    graphSize = sumOf (grNodes . like (1 :: Int))
+
+    values = HashMap.fromList targets
+
+    toValues lst =
+      let keepThese = HashSet.fromList $ lst
+      in HashMap.toList (HashMap.intersection values (HashSet.toMap keepThese))
+
+    logAndTransform coreFp res = do
+      let x = toX res
+      L.debug
+        $ "Reducing to" <-> display (length res)
+        <-> "SCC, containing" <-> display (length x)
+        <-> "classes."
+      return (coreFp ++ x)
+
+    toX :: [([ClassName], [ClassName])] -> [(ClassName, a)]
+    toX = toValues . List.concatMap (view _2)
+
+    -- toSCCs name nm lst = do
+    --   let
+    --     (bad, Set.fromList -> required) =
+    --       partitionEithers
+    --       . toListOf (folded . to (\n -> maybe (Left n) Right $ Map.lookup n nm ))
+    --       $ lst
+
+    --   forM_ bad $ \cn -> do
+    --     L.warn $ "Could not find"
+    --       <-> name
+    --       <-> "class:"
+    --       <-> display (cn ^. fullyQualifiedName) <> "."
+
+    --   L.debug $ "Found that" <-> name <-> "covers" <-> display (Set.size required) <-> "SCC."
+    --   return required
 
 setupPredicate ::
   (HasLogger env, HasConfig env, MonadReader env m, MonadClassPool m, MonadIO m)
-  => [ClassName] -> m (Maybe (PredicateM (ReaderT env IO) [(ClassName, FileContent)]))
-setupPredicate targets = L.phase "Setup Predicate" $ do
+  => ([(ClassName, FileContent)] -> DirTree FileContent)
+  -> [(ClassName, FileContent)]
+  -> m (Maybe (PredicateM (ReaderT env IO) [(ClassName, FileContent)]))
+setupPredicate classesToFiles classData = L.phase "Setup Predicate" $ do
   checkOpt <- view cfgCheckOptions
   Showed (CmdOptionWithoutFormat cmdFn) <- view cfgCmdOptions
   workFolder <- view $ cfgReducerOptions . to workFolder
 
-  folder <- makeAbsolute $ workFolder </> "unpacked"
-  tree <- unpackTargets folder
-
   cmd <- liftIO $ cmdFn (DirInput "classes")
+  prd <- liftRIO $ toPredicateM checkOpt cmd workFolder (classesToFiles classData)
 
-  prd <- liftRIO $ do
-    toPredicateM checkOpt cmd workFolder (dirTree tree)
+  return . fmap (contramap classesToFiles) $ prd
 
-  return $ fmap (contramap $ classesToFiles) prd
+unpackTarget ::
+  (MonadReader s m, MonadIO m, HasLogger s, HasConfig s)
+  => FilePath -> m (DirTree FileContent, DirTree FileContent)
+unpackTarget folder = do
+  L.phase ("Unpacking target to" <-> display folder) $ do
+    target <- view cfgTarget
+    dx <- doesDirectoryExist target
+    if dx
+      then liftIO $ do
+        _ :/ tree <- readTree target
+        writeTreeWith copyFileWithMetadata (folder :/ tree)
+      else liftIO $ do
+        arch <- toArchive <$> BL.readFile target
+        extractFilesFromArchive [OptDestination folder] arch
 
+    -- TODO: Do not save files yet (problems with runtime annotation).
+    -- saveClasses folder targets
+    _ :/ tree <- liftIO $ fmap SameAs <$> readTree folder
+
+    return
+      ( filterTreeOnFiles isClass tree
+      , filterTreeOnFiles (not . isClass) tree
+      )
   where
-    classesToFiles =
-      fromJust . fromFileList . map (_1 %~ relativePathOfClass)
-
-    unpackTargets folder =
-      L.phase ("Unpacking target to" <-> display folder) $ do
-        saveClasses folder targets
-        liftIO $ fmap SameAs <$> readTree folder
+    isClass fn = takeExtension fn == ".class"
 
 liftRIO ::
   (MonadReader env m, MonadIO m)
@@ -170,15 +310,6 @@ liftRIO ::
 liftRIO m = do
   x <- ask
   liftIO $ runReaderT m x
-
-readTargetClasses ::
-  (HasLogger env, HasConfig env, MonadReader env m, MonadIO m)
-  => m [ClassName]
-readTargetClasses = L.phase "Read Target Classses" $ do
-  cls <- liftIO . classes . fromClassPathOnly =<< view (cfgTarget . to (:[]))
-  L.debug $ "Found" <-> display (length cls) <-> "target classes."
-  return $ map fst cls
-
 
 preloadClasses :: ReaderT Config IO ClassPreloader
 preloadClasses = L.phase "Preloading Classes" $ do
