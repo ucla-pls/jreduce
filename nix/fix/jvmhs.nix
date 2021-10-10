a4febcfcc20ff8d7304d990f4d1d40409a851fef
Christian Gram Kalhauge <christian@kalhauge.dk>
2021-10-10 16:27:43 +0200

    Make it work for some more cases

--- nix/fix/jvmhs.nix
+++ nix/fix/jvmhs.nix
@@ -1,7 +1,7 @@
 # Auto-generated with fixnix (version 0.0.0)
 # add gh:ucla-pls/jvmhs
 builtins.fetchTarball {
-  name   = "jvmhs_914390";
-  url    = "https://github.com/ucla-pls/jvmhs/archive/9143907131a5062c9a52c34cee60776a96b9ea1f.tar.gz";
-  sha256 = "10gj78agbwsjkz17kj2davmngvkq5l13ks2j7slbbz2y1nwjd7fn";
+  name   = "jvmhs_188650";
+  url    = "https://github.com/ucla-pls/jvmhs/archive/18865087d573e23057ce3102b90cb8a1c607e1fd.tar.gz";
+  sha256 = "0dv2kz3q8mn4jbr9hamkxlmjhsay89qz2ly577wm19pyk7sljy10";
 }
