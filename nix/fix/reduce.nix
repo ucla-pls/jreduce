f0830583d043ddcbf20b04c9eebf7f06dc8ef4c7
Christian Gram Kalhauge <christian@kalhauge.dk>
2021-10-10 14:42:18 +0200

    Upgrade Reduce

--- nix/fix/reduce.nix
+++ nix/fix/reduce.nix
@@ -1,7 +1,7 @@
 # Auto-generated with fixnix (version 0.0.0)
 # add gh:ucla-pls/reduce
 builtins.fetchTarball {
-  name   = "reduce_4d6d25";
-  url    = "https://github.com/ucla-pls/reduce/archive/4d6d258a4c5df00ef12fc0820354ff796e3dbbd5.tar.gz";
-  sha256 = "16wa9szpzwi5ypyfqcyq9wg3lvgsj5mxq4y7ycfdqhhpl8j9akb0";
+  name   = "reduce_029d39";
+  url    = "https://github.com/ucla-pls/reduce/archive/029d3942383316111340a5fd9bdbe19276265c37.tar.gz";
+  sha256 = "14xvs2mph834z1nm0v39pb5zsm948rryj9nmx8sm33yp9r809ckg";
 }
